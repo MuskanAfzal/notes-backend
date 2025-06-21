5da7deffd5529ba886f716f5d97cfbe28f08cec6
Muskan Afzal <148281538+MuskanAfzal@users.noreply.github.com>
2025-06-22 00:07:05 +0500

    added frontend link

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
 }));
+
 
 app.use(express.json());
 
